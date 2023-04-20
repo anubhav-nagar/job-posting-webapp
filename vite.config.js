64c539cbcc131396656d18d3090b7d0ea38e5eb6
Anubhav <anubhavnagar009@gmail.com>
2023-04-21 00:31:20 +0530

    updated the base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/anubhav-nagar/job-posting-webapp",
+  base: "/job-posting-webapp/",
   plugins: [react()],
   server: {
     port: 3000
